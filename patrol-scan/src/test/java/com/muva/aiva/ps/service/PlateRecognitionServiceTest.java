2234a1f478fdf9e5d30fd1bea12e2faaeceeb516
Angélica Guamán Albarracín <angelica.guaman.albarracin@gmail.com>
2025-04-17 16:25:22 +0200

    test: se actualizan los tests para probar la biblioteca

--- patrol-scan/src/test/java/com/muva/aiva/ps/service/PlateRecognitionServiceTest.java
+++ patrol-scan/src/test/java/com/muva/aiva/ps/service/PlateRecognitionServiceTest.java
@@ -1,53 +1,73 @@
 package com.muva.aiva.ps.service;
 
 import com.muva.aiva.ps.service.impl.PlateRecognitionServiceImpl;
-import com.muva.aiva.ps.service.impl.RunPythonServiceImpl;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 //@ExtendWith(MockitoExtension.class)
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 class PlateRecognitionServiceTest {
 
-  //  @InjectMocks
+    //@InjectMocks
+    @Autowired
     private PlateRecognitionServiceImpl plateRecognitionService;
 
     //@Mock
-    private RunPythonService runPythonService;
+    //@Autowired
+    //private RunPythonService runPythonService;
 
     @BeforeEach
     void setUp() {
-        runPythonService = new RunPythonServiceImpl();
-        plateRecognitionService = new PlateRecognitionServiceImpl(runPythonService);
+        //runPythonService = new RunPythonServiceImpl();
+        //plateRecognitionService = new PlateRecognitionServiceImpl(runPythonService);
 
     }
 
+    @SneakyThrows
     @Test
     void testRecognizePlate_ValidImage() {
-        File testImage = new File("test_image.jpg");
 
-        Optional<String> result = plateRecognitionService.recognizePlate(testImage);
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("data/frame0076.png");
+
+        if (inputStream == null) {
+            throw new FileNotFoundException("El archivo no se encontró en resources");
+        }
+
+        File tempFile = File.createTempFile("data/frame0076", ".png");
+        tempFile.deleteOnExit();
+
+        Files.copy(inputStream, tempFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+
+        Optional<String> result = plateRecognitionService.recognizePlate(tempFile);
 
         assertTrue(result.isPresent());
         assertEquals("1234ABC", result.get());
     }
 
     @Test
+    @SneakyThrows
     void testRecognizePlate_InvalidImage() {
         File testImage = new File("empty.jpg");
 
         Optional<String> result = plateRecognitionService.recognizePlate(testImage);
 
-        assertFalse(result.isEmpty());
+        assertNull(result);
     }
 
     @Test
